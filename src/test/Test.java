be37169642b6857f1e9e17bc521e7b9235880bcb
keanlim8 <keanlim58@gmail.com>
2021-02-05 15:26:04 -0500

    first commit from eclipse

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "no";
+		
+		if (hungry == "yes")
+			System.out.println("eat something");
+		else {
+			System.out.println("do work");
+		}
+	}
+
+}
